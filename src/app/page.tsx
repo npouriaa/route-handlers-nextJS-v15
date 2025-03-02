9d3530315153be1bb44ef438e5efa662ebe1b921
Pouria Navipour <npouriaa@gmail.com>
2025-03-02 20:53:21 +0330

    rename component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import DataTable from "@/components/DataTable";
+import UserList from "@/components/UserList";
 
 export default async function Home() {
   return (
     <div className="p-9 flex items-center flex-col gap-7 w-full">
       <h1 className="text-3xl font-bold">Route handlers example Next Js v15</h1>
-      <DataTable />
+      <UserList />
     </div>
   );
 }
